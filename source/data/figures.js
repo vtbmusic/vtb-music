4a515ee2487a985ed1bf100f7673d88074da4d0a
Santiego <sandiegomail@qq.com>
2020-04-02 22:56:40 +0800

    update source/data/figures.js.

--- source/data/figures.js
+++ source/data/figures.js
@@ -20,6 +20,26 @@ var data_data_figures = [
 		}
 	},
 	{
+		"name": "天音彼方",
+		"jpname": "天音かなた",
+		"img": "天音彼方.png",
+		"group": ["hololive"],
+		"links":{
+			"Bilibili": "https://space.bilibili.com/491474048",
+			"Youtube": "https://www.youtube.com/channel/UCZlDXzGoo7d44bwdNObFacg",
+		}
+	},
+    {
+		"name": "神乐七奈",
+		"jpname": "かぐらなな",
+		"img": "神乐七奈.png",
+		"group": [],
+		"links":{
+			"Bilibili": "https://space.bilibili.com/386900246",
+			"Youtube": "https://www.youtube.com/channel/UCbfv8uuUXt3RSJGEwxny5Rw",
+		}
+	},
+    {
 		"name": "赤井心",
 		"jpname": "赤井 はあと",
 		"img": "赤井心.png",
@@ -37,46 +57,6 @@ var data_data_figures = [
 		"links":{
 			"Bilibili": "https://space.bilibili.com/332704117/",
 			"Youtube": "https://www.youtube.com/channel/UCdn5BQ06XqgXoAxIhbqw5Rg",
-		}
-	},
-	{
-		"name": "天音彼方",
-		"jpname": "天音かなた",
-		"img": "天音彼方.png",
-		"group": ["hololive"],
-		"links":{
-			"Bilibili": "https://space.bilibili.com/491474048",
-			"Youtube": "https://www.youtube.com/channel/UCZlDXzGoo7d44bwdNObFacg",
-		}
-	},
-	{
-		"name": "百鬼绫目",
-		"jpname": "百鬼 あやめ",
-		"img": "百鬼绫目.png",
-		"group": ["hololive"],
-		"links":{
-			"Bilibili": "https://space.bilibili.com/389858027",
-			"Youtube": "https://www.youtube.com/channel/UC7fk0CB07ly8oSl0aqKkqFg",
-		}
-	},
-	{
-		"name": "律可",
-		"jpname": "りっか",
-		"img": "律可.png",
-		"group": ["hololive"],
-		"links":{
-			"Bilibili": "https://space.bilibili.com/350631685",
-			"Youtube": "https://www.youtube.com/channel/UC9mf_ZVpouoILRY9NUIaK-w",
-		}
-	},
-    {
-		"name": "神乐七奈",
-		"jpname": "かぐらなな",
-		"img": "神乐七奈.png",
-		"group": [],
-		"links":{
-			"Bilibili": "https://space.bilibili.com/386900246",
-			"Youtube": "https://www.youtube.com/channel/UCbfv8uuUXt3RSJGEwxny5Rw",
 		}
 	},
 	{
@@ -109,14 +89,14 @@ var data_data_figures = [
 			"Youtube": "https://www.youtube.com/channel/UC8NZiqKx6fsDT3AVcMiVFyA",
 		}
 	},
-	{
-		"name": "鹿乃",
-		"jpname": "かの",
-		"img": "鹿乃.png",
-		"group": ["花寄女子寮"],
+    {
+		"name": "百鬼绫目",
+		"jpname": "百鬼 あやめ",
+		"img": "百鬼绫目.png",
+		"group": ["hololive"],
 		"links":{
-			"Bilibili": "https://space.bilibili.com/316381099",
-			"Youtube": "https://www.youtube.com/channel/UCfuz6xYbYFGsWWBi3SpJI1w",
+			"Bilibili": "https://space.bilibili.com/389858027",
+			"Youtube": "https://www.youtube.com/channel/UC7fk0CB07ly8oSl0aqKkqFg",
 		}
 	},
     {
@@ -127,6 +107,86 @@ var data_data_figures = [
 		"links":{
 			"Bilibili": "https://space.bilibili.com/389857640",
 			"Youtube": "https://www.youtube.com/channel/UCXTpFs_3PqI41qX2d9tL2Rw",
+		}
+	},
+    {
+		"name": "鹿乃",
+		"jpname": "かの",
+		"img": "鹿乃.png",
+		"group": ["花寄女子寮"],
+		"links":{
+			"Bilibili": "https://space.bilibili.com/316381099",
+			"Youtube": "https://www.youtube.com/channel/UCfuz6xYbYFGsWWBi3SpJI1w",
+		}
+	},
+    {
+		"name": "时乃空",
+		"jpname": "ときのそら",
+		"img": "时乃空.jpg",
+		"group": ["hololive"],
+		"links":{
+			"Bilibili": "https://space.bilibili.com/286179206",
+			"Youtube": "https://www.youtube.com/channel/UCp6993wxpyDPHUpavwDFqgg",
+		}
+	},
+    {
+		"name": "AZKi",
+		"jpname": "",
+		"img": "AZKi.png",
+		"group": ["hololive"],
+		"links":{
+			"Bilibili": "https://space.bilibili.com/389056211",
+			"Youtube": "https://www.youtube.com/channel/UC0TXe_LYZ4scaW2XMyi5_kw",
+		}
+	},
+    {
+		"name": "大空昴",
+		"jpname": "大空 スバル",
+		"img": "大空昴.png",
+		"group": ["hololive"],
+		"links":{
+			"Bilibili": "https://space.bilibili.com/389859190",
+			"Youtube": "https://www.youtube.com/channel/UCvzGlP9oQwU--Y0r9id_jnA",
+		}
+	},
+    {
+		"name": "萝卜子",
+		"jpname": "Roboko-san",
+		"img": "萝卜子.png",
+		"group": ["hololive"],
+		"links":{
+			"Bilibili": "https://space.bilibili.com/20813493",
+			"Youtube": "https://www.youtube.com/channel/UCDqI2jOz0weumE8s7paEk6g",
+		}
+	},
+    {
+		"name": "夜空梅露",
+		"jpname": "夜空 メル",
+		"img": "夜空梅露.png",
+		"group": ["hololive"],
+		"links":{
+			"Bilibili": "https://space.bilibili.com/389856447",
+			"Youtube": "https://www.youtube.com/channel/UCD8HOxPs4Xvsm8H0ZxXGiBw",
+		}
+	},
+    {
+		"name": "樱巫女",
+		"jpname": "Sakura Miko",
+		"img": "樱巫女.png",
+		"group": ["hololive"],
+		"links":{
+			"Bilibili": "https://space.bilibili.com/366690056",
+			"Youtube": "https://www.youtube.com/channel/UC-hM6YJuNYVAmUWxeIr9FeA",
+		}
+	},
+    {
+		"name": "律可",
+		"jpname": "りっか",
+		"img": "律可.png",
+		"group": ["hololive"],
+		"links":{
+			"Bilibili": "https://space.bilibili.com/350631685",
+			"Youtube": "https://www.youtube.com/channel/UC9mf_ZVpouoILRY9NUIaK-w",
 		}
 	},
 	{
@@ -175,16 +235,6 @@ var data_data_figures = [
 		"links":{
 			"Bilibili": "https://space.bilibili.com/380829248",
 			"Youtube": "https://www.youtube.com/channel/UCRXBTd80F5IIWWY4HatJ5Ug",
-		}
-	},
-    {
-		"name": "时乃空",
-		"jpname": "ときのそら",
-		"img": "时乃空.jpg",
-		"group": ["hololive"],
-		"links":{
-			"Bilibili": "https://space.bilibili.com/286179206",
-			"Youtube": "https://www.youtube.com/channel/UCp6993wxpyDPHUpavwDFqgg",
 		}
 	},
 ]
