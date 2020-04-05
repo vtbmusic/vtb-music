3f67a5bd071c588984cf1bad8530c1047f46369b
清楚担当 <7407894+mima4561834@user.noreply.gitee.com>
2020-04-05 15:21:35 +0800

    天曰ひよ

--- source/data/figures.js
+++ source/data/figures.js
@@ -30,6 +30,16 @@ var data_data_figures = [
 		}
 	},
     {
+		"name": "天曰ひよ",
+		"jpname": "天曰ひよ",
+		"img": "天曰ひよ.png",
+		"group": [],
+		"links":{
+			"Bilibili": "https://space.bilibili.com/27403816",
+			"Youtube": "https://www.youtube.com/channel/UC04QM30wwa0qMs101Mwz29w",
+		}
+	},
+    {
 		"name": "神乐七奈",
 		"jpname": "かぐらなな",
 		"img": "神乐七奈.png",
